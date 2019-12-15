2946701d778d50d7e70c2d00e7f2f67088bda3f8
ojoc <ojoc@users.noreply.github.com>
2019-12-15 23:24:19 +0000

    configurations: configurations -> cfg

--- configurations/configurations.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import yaml
-
-directory = os.path.split(os.path.abspath(__file__))[0]
-
-with open(os.path.join(directory, 'variables.yml'), 'r') as stream:
-    variables = yaml.safe_load(stream)
