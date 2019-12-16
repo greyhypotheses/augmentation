eaa20d4598dee8684710caec83f55455c5e0f22b
ojoc <ojoc@users.noreply.github.com>
2019-12-16 00:10:47 +0000

    src/main.py: Updating

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-import configurations.configurations as cfg
+import cfg.cfg as cfg
+import os
+import src.data.steps as steps
 
 
 def main():
 
-    remnant = cfg.variables['image']['remnant']
-    strip = int(remnant/2)
-    input_size = (cfg.variables['image']['rows'], cfg.variables['image']['columns'])
-    temporary_size = (input_size[0] + remnant, input_size[1] + remnant)
-    rows, columns = temporary_size[0], temporary_size[1]
-    centre = (columns/2, rows/2)
-    rotations = cfg.variables['image']['rotations']
+    var = cfg.Cfg().variables()
+    rows = var.image.rows
 
-    print(type(rotations[1]))
+    print(rows)
+    print(os.getcwd())
+
+    steps.Steps().augment()
 
 
 if __name__ == '__main__':
