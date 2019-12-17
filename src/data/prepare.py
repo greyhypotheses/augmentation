b56a747b82b12573716e0cf26ef4e5c0a12139d8
ojoc <ojoc@users.noreply.github.com>
2019-12-17 04:00:51 +0000

    src/data/prepare.py: Updating

--- src/data/prepare.py
+++ src/data/prepare.py
@@ -11,27 +11,43 @@ class Prepare:
         self.use = variables.source.metadata.use
         self.alt = variables.source.metadata.alt
 
-    def missing(self, inventory):
+    def missing(self, data):
         """
-        Addressing missing values
-        :param inventory:
+        Addressing missing values.
+        Erroneous:
+            data[self.use[i]] = data[self.use[i]].where(data[self.use[i]] != '', other=self.alt[i])
+
+        :type data: pandas.DataFrame
+
+        :param data:
         :return:
         """
 
         for i in range(len(self.use)):
-            inventory[self.use[i]] = inventory[self.use[i]].fillna(value=self.alt[i])
-            inventory[self.use[i]] = inventory[self.use[i]].where(inventory[self.use[i]] != '', other=self.alt[i])
+            data[self.use[i]] = data[self.use[i]].fillna(value=self.alt[i])
+            data[self.use[i]] = data[self.use[i]].apply(lambda x: self.alt[i] if x == '' else x)
 
-        return inventory
+        return data
 
-    def angles(self, inventory, fields, labels):
+    def angles(self, data, fields, labels):
+        """
+        Expands the data frame such that each original image now has a row per rotation required.
 
-        angle_fields = {'A' + str(i).zfill(3):i for i in self.rotations}
+        :type data: pandas.DataFrame
+        :type fields: list
+        :type labels: list
 
-        inventory = inventory.assign(**angle_fields)
-        inventory = inventory.melt(id_vars=fields + labels,
-                                   value_vars=list(angle_fields.keys()),
-                                   var_name=['angle_fields_names'],
-                                   value_name='angle').drop(columns=['angle_fields_names'])
+        :param data: A data frame
+        :param fields: The columns of data that are not label/class columns
+        :param labels: The label/class columns of data
+        :return:
+        """
+        angle_fields = {'A' + str(i).zfill(3): i for i in self.rotations}
 
-        return inventory
+        data = data.assign(**angle_fields)
+        data = data.melt(id_vars=fields + labels,
+                         value_vars=list(angle_fields.keys()),
+                         var_name=['angle_fields_names'],
+                         value_name='angle').drop(columns=['angle_fields_names'])
+
+        return data
