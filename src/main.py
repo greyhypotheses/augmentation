53e59a189e3baee34693a39b6d1071173deeb57c
ojoc <ojoc@users.noreply.github.com>
2019-12-20 20:22:24 +0000

    src/main.py: In progress

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import glob
+import logging
 import os
 
 import dask.array as da
+import pandas as pd
 
 import cfg.cfg as cfg
 import src.data.generator as generator
@@ -9,30 +11,49 @@ import src.data.prepare as prepare
 import src.data.usable as usable
 
 
-def main():
-    # A summary of images metadata & labels, a list of the label fields, and a list of the metadata fields
-    u = usable.Usable()
-    inventory, labels, fields = u.summary()
+class Main:
 
-    # Prepare
-    p = prepare.Prepare()
-    inventory = p.missing(inventory)
-    inventory = p.angles(inventory, labels=labels, fields=fields)
+    def __init__(self):
+        self.name = 'Main'
 
-    # Names
-    variables = cfg.Cfg().variables()
-    template = inventory[['image', 'angle']].sample(n=64, random_state=5)
-    template['filename'] = template.image. \
-        apply(lambda x: variables.source.images.url + x + variables.source.images.ext)
+        variables = cfg.Cfg().variables()
+        self.path = variables['target']['images']['path']
 
-    # Existing augmentations
-    states = [os.remove(f) for f in glob.glob(os.path.join(variables.target.images.path, '*.png'))]
-    print(states)
+    def main(self):
 
-    # Augment
-    for image, angle, filename in da.from_array(template.to_numpy(), chunks=16):
-        generator.Generator().images(filename=filename.compute(), angle=angle.compute())
+        # Logging
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger(__name__)
+
+        # A summary of images metadata & labels, a list of the label fields, and a list of the metadata fields
+        use = usable.Usable()
+        inventory, labels, fields = use.summary()
+
+        # Prepare
+        prep = prepare.Prepare()
+        inventory = prep.missing(inventory)
+        inventory = prep.angles(inventory, labels=labels, fields=fields)
+        inventory = prep.outliers(inventory)
+        inventory = prep.filename(inventory)
+        logger.info(inventory.shape)
+        logger.info(inventory.info())
+
+        # Delete existing augmentations
+        for f in glob.glob(os.path.join(self.path, '*.png')):
+            os.remove(f)
+
+        # Augment
+        template = inventory[['filename', 'angle']]
+        outcomes = [generator.Generator().images(filename=filename.compute(), angle=angle.compute())
+                    for filename, angle in da.from_array(template.to_numpy()[:16], chunks=16)]
+
+        augmentations = pd.DataFrame(outcomes, columns=['image', 'angle', 'drawn'])
+        focus = inventory.merge(augmentations, how='inner', on=['image', 'angle']).drop(columns=['filename'])
+        logger.info(focus.head())
+
+        # Write
+        focus.to_csv('inventory.csv')
 
 
 if __name__ == '__main__':
-    main()
+    Main().main()
