f370b72787d865d052fe0af8faa196f7178b48b8
ojoc <ojoc@users.noreply.github.com>
2019-12-21 07:37:31 +0000

    src/main.py: In progress

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+"""Module main"""
 import glob
 import logging
 import os
@@ -11,49 +12,53 @@ import src.data.prepare as prepare
 import src.data.usable as usable
 
 
-class Main:
+def main():
+    """
+    main
 
-    def __init__(self):
-        self.name = 'Main'
+    :return:
+    """
+    variables = cfg.Cfg().variables()
+    path = variables['target']['images']['path']
 
-        variables = cfg.Cfg().variables()
-        self.path = variables['target']['images']['path']
+    # Logging
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
 
-    def main(self):
+    # A summary of images metadata & labels, a list of the label fields, and a list of the metadata fields
+    use = usable.Usable()
+    inventory, labels, fields = use.summary()
 
-        # Logging
-        logging.basicConfig(level=logging.DEBUG)
-        logger = logging.getLogger(__name__)
+    # Prepare
+    prep = prepare.Prepare()
+    inventory = prep.missing(inventory)
+    inventory = prep.angles(inventory, labels=labels, fields=fields)
 
-        # A summary of images metadata & labels, a list of the label fields, and a list of the metadata fields
-        use = usable.Usable()
-        inventory, labels, fields = use.summary()
+    # Unnecessary augmentations
+    inventory = inventory.loc[~((inventory.NV == 1) & (inventory.angle > 0))]
+    inventory.reset_index(inplace=True)
 
-        # Prepare
-        prep = prepare.Prepare()
-        inventory = prep.missing(inventory)
-        inventory = prep.angles(inventory, labels=labels, fields=fields)
-        inventory = prep.outliers(inventory)
-        inventory = prep.filename(inventory)
-        logger.info(inventory.shape)
-        logger.info(inventory.info())
+    # Image URL
+    inventory = prep.filename(inventory)
+    logger.info(inventory.shape)
+    logger.info(inventory.info())
 
-        # Delete existing augmentations
-        for f in glob.glob(os.path.join(self.path, '*.png')):
-            os.remove(f)
+    # Delete existing augmentations
+    for file in glob.glob(os.path.join(path, '*.png')):
+        os.remove(file)
 
-        # Augment
-        template = inventory[['filename', 'angle']]
-        outcomes = [generator.Generator().images(filename=filename.compute(), angle=angle.compute())
-                    for filename, angle in da.from_array(template.to_numpy()[:128], chunks=16)]
+    # Augment
+    template = inventory[['filename', 'angle']]
+    outcomes = [generator.Generator().images(filename=filename.compute(), angle=angle.compute())
+                for filename, angle in da.from_array(template.to_numpy()[:16], chunks=16)]
 
-        augmentations = pd.DataFrame(outcomes, columns=['image', 'angle', 'drawn'])
-        focus = inventory.merge(augmentations, how='inner', on=['image', 'angle']).drop(columns=['filename'])
-        logger.info(focus.head())
+    augmentations = pd.DataFrame(outcomes, columns=['image', 'angle', 'drawn'])
+    focus = inventory.merge(augmentations, how='inner', on=['image', 'angle']).drop(columns=['filename'])
+    logger.info(focus.head())
 
-        # Write
-        focus.to_csv('inventory.csv')
+    # Write
+    focus.to_csv('inventory.csv')
 
 
 if __name__ == '__main__':
-    Main().main()
+    main()
