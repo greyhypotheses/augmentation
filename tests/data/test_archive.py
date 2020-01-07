cfe95e2d3b7c471557450fdfb3a5e4c431e55755
ojoc <ojoc@users.noreply.github.com>
2020-01-07 15:50:05 +0000

    src/data/archive.py & tests/data/test_archive.py have been decommissioned.

--- tests/data/test_archive.py
+++
@@ -1,7 +0,0 @@
-import src.data.archive as archive
-
-
-class TestArchive:
-
-    def test_images(self):
-        assert len(archive.Archive().images()) != 0, "There are no images in the ISIC Archive"
