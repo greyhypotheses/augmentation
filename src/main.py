aa05b4cd465df8855b55e2673d62b979edd0ffdf
ojoc <ojoc@users.noreply.github.com>
2020-01-07 16:01:10 +0000

    src/main.py: In progress

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 """Module main"""
 
-import glob
-import logging
-import multiprocessing as mp
 import os
 import sys
 
@@ -16,6 +13,7 @@ if __name__ == '__main__':
     import src.cfg.cfg as cfg
     import src.data.prepare as prepare
     import src.data.usable as usable
+    import src.io.directories as dir
 
 
 def main():
@@ -25,14 +23,12 @@ def main():
     :return:
     """
 
+    # Clear the current set of images
+    dir.Directories().clear()
+
     # Variables
     variables = cfg.Cfg().variables()
     path = variables['target']['images']['path']
-
-    # Logging
-    # logging.basicConfig(level=logging.DEBUG)
-    logging.disable(logging.DEBUG)
-    logger = logging.getLogger(__name__)
 
     # A summary of images metadata & labels, a list of the label fields, and a list of the metadata fields
     use = usable.Usable()
@@ -50,23 +46,24 @@ def main():
     # Image URL
     inventory = prep.filename(inventory)
 
-    # Delete existing augmentations
-    pool = mp.Pool(mp.cpu_count())
-    images_list = glob.glob(os.path.join(path, '*.png'))
-    images_iterable = [{images_list[i]} for i in range(len(images_list))]
-    pool.starmap(os.remove, images_iterable)
+    # Development: This snippet will be deleted after the development phase
+    development = True
+    if development:
+        inventory = inventory.groupby(['AK', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'SCC', 'VASC'])[inventory.columns.tolist()] \
+            .apply(lambda x: x.sample(n=160, replace=False, random_state=5))
+        inventory.reset_index(drop=True, inplace=True)
 
-    # Augment
-    if not os.path.exists(path):
-        os.makedirs(path)
+    # Directory for augmentations
+    dir.Directories().create()
 
+    # Augmentation
     template = inventory[['filename', 'angle']]
+
     outcomes = [generator.Generator().images(filename=filename.compute(), angle=angle.compute())
-                for filename, angle in da.from_array(template.to_numpy()[:64], chunks=32)]
+                for filename, angle in da.from_array(template.to_numpy(), chunks=64)]
 
     augmentations = pd.DataFrame(outcomes, columns=['name', 'image', 'angle', 'drawn'])
     focus = inventory.merge(augmentations, how='inner', on=['image', 'angle']).drop(columns=['filename', 'index'])
-    logger.info(focus.head())
 
     # Write
     # Automate directories for zipping via https://docs.python.org/3.7/library/shutil.html
