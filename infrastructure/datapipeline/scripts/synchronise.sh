d8e9bf8f039c6d6e17708c4d8e0fea96a8a7e956
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-19 16:09:08 +0000

    Update infrastructure/datapipeline/scripts/synchronise.sh
    Edits comments.
    
    Modifies: [infrastructure/datapipeline/scripts/synchronise.sh]

--- infrastructure/datapipeline/scripts/synchronise.sh
+++ infrastructure/datapipeline/scripts/synchronise.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~
 
-# Delivering the results files to Amazon S3
+# Directory name based on square image dimensions
 directory=$1.$1
+
+# Delivering the results files to Amazon S3
 aws s3 rm s3://deep.learning.images/$directory/ --recursive
 aws s3 sync ~/images/zips/ s3://deep.learning.images/$directory/zips/
 aws s3 cp ~/images/inventory.csv s3://deep.learning.images/$directory/
