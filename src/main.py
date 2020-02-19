7085b70263b8ff2608b4278b78c34bbf070314ed
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-19 20:55:28 +0000

    Update src/main.py: Update the arguments parsing section
    The section now depends on src/io/arguments.py
    
    Modifies: [src/main.py]

--- src/main.py
+++ src/main.py
@@ -10,12 +10,12 @@ import pandas as pd
 if __name__ == '__main__':
     sys.path.append(os.getcwd())
     sys.path.append(os.path.join(os.getcwd(), 'src'))
-    import config
     import src.data.generator as generator
     import src.data.prepare as prepare
     import src.data.usable as usable
     import src.data.preserve as preserve
     import src.io.directories as directories
+    import src.io.arguments as arguments
 
 
 def main():
@@ -27,24 +27,12 @@ def main():
 
     # Parse the expected input argument
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_length", help="The number of rows or columns of the prospective square images; "
-                                             "all pre-trained convolution neural network bases require square "
-                                             "image inputs.")
+    parser.add_argument("image_length", type=arguments.Arguments().image_length,
+                        help="The number of rows or columns of the prospective square images; "
+                             "all pre-trained convolution neural network bases require square image inputs.")
+    parser.add_argument("-p", "--preview", type=arguments.Arguments().preview,
+                        help="Augment a specified, small, number of images")
     args = parser.parse_args()
-
-    # Validate the argument's type
-    try:
-        isinstance(int(args.image_length), int)
-        image_length = int(args.image_length)
-    except TypeError as exception:
-        print(exception)
-        sys.exit(1)
-
-    # Ensure that the 'image_length' argument is greater than the general minimum expected.
-    variables = config.Config().variables()
-    minimum_length = variables['augmentation']['images']['minimum_length']
-    if image_length < minimum_length:
-        raise Exception("The length of each prospective square image must be at least {}".format(minimum_length))
 
     # Clear the current set of images
     directories.Directories().clear()
@@ -55,13 +43,19 @@ def main():
     # Prepare
     inventory = prepare.Prepare().summary(data=inventory, fields=fields, labels=labels)
 
+    # If preview has a value
+    if args.preview is not None:
+        limit = args.preview
+    else:
+        limit = inventory.shape[0]
+
     # Directory for augmentations
     directories.Directories().create()
 
     # Augmentation
-    outcomes = [generator.Generator(image_length=image_length).images(
+    outcomes = [generator.Generator(image_length=args.image_length).images(
         filename=image_url.compute(), angle=angle.compute()
-    ) for image_url, angle in da.from_array(inventory[['image_url', 'angle']].to_numpy(), chunks=(64, 2))]
+    ) for image_url, angle in da.from_array(inventory[['image_url', 'angle']].to_numpy()[:limit], chunks=(64, 2))]
     augmentations = pd.DataFrame(outcomes, columns=['name', 'image', 'angle', 'drawn'])
 
     # Save inventory, split the directory of image files into smaller directories, zip
