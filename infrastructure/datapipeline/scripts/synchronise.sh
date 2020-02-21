3875c61e23d85d2c461590cb7bba74fd0edce945
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-21 12:45:21 +0000

    Update synchronise.sh: Switch from '~' -> '$HOME'
    Modifies: [infrastructure/datapipeline/scripts/synchronise.sh]

--- infrastructure/datapipeline/scripts/synchronise.sh
+++ infrastructure/datapipeline/scripts/synchronise.sh
@@ -6,5 +6,5 @@ directory=$1.$1
 
 # Delivering the results files to Amazon S3.
 aws s3 rm s3://deep.learning.images/$directory/ --recursive
-aws s3 sync ~/images/zips/ s3://deep.learning.images/$directory/zips/
-aws s3 cp ~/images/inventory.csv s3://deep.learning.images/$directory/
+aws s3 sync $HOME/images/zips/ s3://deep.learning.images/$directory/zips/
+aws s3 cp $HOME/images/inventory.csv s3://deep.learning.images/$directory/
