f468d5e2ccc4c67b2462aeb74fc7b73f016168c0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-06-16 18:01:08 +0100

    Update tests/data/test_sources.py

--- tests/data/test_sources.py
+++ tests/data/test_sources.py
@@ -1,35 +1,49 @@
-import config
-import src.data.sources as sources
+import pytest
 
-variables = config.Config().variables()
+import src.data.sources
+import src.data.sources
+import src.io.arguments
+import src.io.arguments
 
 
 class TestSources:
 
-    def test_truth(self):
-        truth_use = variables['source']['truth']['use']
+    @pytest.fixture()
+    def var(self):
+        arguments = src.io.arguments.Arguments()
+        urlstring = 'https://raw.githubusercontent.com/greyhypotheses/dictionaries/develop/augmentation/variables.yml'
 
-        truth = sources.Sources().truth()
+        req = arguments.url(urlstring)
+        return arguments.parameters(req)
+
+    @pytest.fixture()
+    def sources(self, var):
+        return src.data.sources.Sources(var=var)
+
+    def test_truth(self, sources, var):
+        truth_use = var.source.truth.use
+
+        truth = sources.truth()
         assert truth.shape[0] != 0, "The ground truth labels file should not be empty"
         assert set(truth.columns) == set(truth_use), "The columns of truth must be those " \
                                                      "specified in variables.yml"
 
-    def test_metadata(self):
-        metadata_use = variables['source']['metadata']['use']
+    def test_metadata(self, sources, var):
+        metadata_use = var.source.metadata.use
 
-        metadata = sources.Sources().metadata()
+        metadata = sources.metadata()
         assert metadata.shape[0] != 0, "The metadata file should not be empty"
         assert set(metadata.columns) == set(metadata_use), "The columns of metadata must be those " \
                                                            "specified in variables.yml"
 
-    def test_summary(self):
-        truth_use = variables['source']['truth']['use']
-        truth_key = variables['source']['truth']['key']
-        metadata_use = variables['source']['metadata']['use']
+    def test_exc(self, sources, var):
+        truth_use = var.source.truth.use
+        truth_key = var.source.truth.key
+        metadata_use = var.source.metadata.use
 
-        truth = sources.Sources().truth()
-        metadata = sources.Sources().metadata()
-        inventory, fields, labels = sources.Sources().summary()
+        truth = sources.truth()
+        metadata = sources.metadata()
+        inventory, fields, labels = sources.exc()
 
         assert truth.shape[0] == inventory.shape[0], "The number of ground truth & inventory data points must be equal"
         assert metadata.shape[0] == inventory.shape[0], "The number of medata & inventory data points must be equal"
